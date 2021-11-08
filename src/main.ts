adb231ade4e48031b57d9b051d8a67150359d3ea
chenzhiqing01 <zombieczq@gmail.com>
2021-11-08 18:37:59 +0800

    feat: action v0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,103 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import * as io from '@actions/io'
+import QRCode from 'qrcode'
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // 0. get params from workflow
+    const env = process.env
+    core.info(`env: ${env}`)
+    const respository = env['GITHUB_REPOSITORY']
+    const ref = env['GITHUB_REF']
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.info(`payload: ${payload}`)
+    const publicPath = core.getInput('PUBLICPATH') || 'https://cdn.jsdelivr.net/gh'
+    const tag = core.getInput('TAG') || ref?.replace(/^refs\/(heads|tags)\//, '')
+    const prefix = `${publicPath}/${respository}@${tag}/`
+    const iosBundlePath = core.getInput('IOSBUNDLEPATH') || 'ios/main.js'
+    const androidBundlePath = core.getInput('ANDROIDBUNDLEPATH') || 'android/main.js'
+    const appName = core.getInput('APPNAME') || ''
+    const logo = core.getInput('LOGO') || ''
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // 1. install node modules
+    let yarnPath = 'yarn'
+    try {
+      yarnPath = await io.which('yarn', true)
+    } catch (error) {
+      core.debug('Please install yarn in global.')
+    }
+    await execDebug(yarnPath)
 
-    core.setOutput('time', new Date().toTimeString())
+    // 2. build ios bundle
+    const bundles = []
+    bundles.push({
+      platform: 'ios',
+      bundlePath: iosBundlePath,
+      qrPath: 'qrcode/ios.png'
+    })
+
+    // 3. build android bundle
+    bundles.push({
+      platform: 'android',
+      bundlePath: androidBundlePath,
+      qrPath: 'qrcode/android.png'
+    })
+
+    // 4. run
+    for (const bundle of bundles) {
+      await execDebug(`yarn build:rn --reset-cache --platform ${bundle.platform}`)
+      const bundleUrl = `${prefix}/${bundle.bundlePath}`
+      core.info(bundleUrl)
+      const qrText = `taro://releases?url=${encodeURIComponent(bundleUrl)}&name=${encodeURIComponent(appName)}&logo=${encodeURIComponent(logo)}`
+      genQr(qrText, bundle.qrPath)
+    }
+
+    // 5. tag
+    await execDebug(`git tag -d ${tag}`)
+    await execDebug(`git push origin :refs/tags/${tag}`)
+    await execDebug(`git add .`)
+    await execDebug(`git commit -m "update by github actions"`)
+    await execDebug(`git tag ${tag}`)
+    await execDebug(`git push origin ${tag}`)
+
+    // 6.release
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof SyntaxError) {
+      core.setFailed(error.message)
+    }
   }
+}
+
+function genQr(text: string, path: string): void {
+  const QR_CODE_PNG_PATH = `${process.cwd()}/${path}`
+  QRCode.toFile(QR_CODE_PNG_PATH, text, {type: 'png'}, err => {
+    if (err) throw err
+    core.info(`generated: ${text} to ${path}`)
+  })
+}
+
+async function execDebug(command: string, args: string[] = []): Promise<void> {
+  const stdout: string[] = []
+  const stderr: string[] = []
+
+  const options: exec.ExecOptions = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdout.push(data.toString())
+      },
+      stderr: (data: Buffer) => {
+        stderr.push(data.toString())
+      }
+    }
+  }
+  core.startGroup(`execute ${command}`)
+  await exec.exec(command, args, options)
+
+  core.debug(stdout.join(''))
+  core.debug(stderr.join(''))
+  core.endGroup()
 }
 
 run()
