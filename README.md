835c552206f2ce49cf98753bcb7d3c3cbccb6060
chenzhiqing01 <zombieczq@gmail.com>
2021-11-12 16:59:31 +0800

    feat: add trn release action

--- README.md
+++ README.md
@@ -1,105 +1,56 @@
-<p align="center">
-  <a href="https://github.com/actions/typescript-action/actions"><img alt="typescript-action status" src="https://github.com/actions/typescript-action/workflows/build-test/badge.svg"></a>
-</p>
+# Github action for bundle taro react native project
 
-# Create a JavaScript Action using TypeScript
+Use this action to package your [Taro React Native](https://docs.taro.zone/) project and generate bundles for Android and iOS. Others can use [Taro Playground APP](https://github.com/wuba/taro-playground) to load the bundle for project preview.
 
-Use this template to bootstrap the creation of a TypeScript action.:rocket:
+Make it easier to share your ideas.
 
-This template includes compilation support, tests, a validation workflow, publishing, and versioning guidance.  
+**Note:** Your React Native version and dependencies must be compatible with the [Taro Playground APP](https://github.com/wuba/taro-playground/blob/main/package.json). 
+## Workflow
 
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
+1. Execute `yarn build:rn` to build the bundle.
+2. Generate a [Taro Playground APP](https://github.com/wuba/taro-playground) protocol, including platform, name, logo and bundle url.
+3. Export the protocol to a file with a QR code.
+4. Commit the changes.
+5. If `GITHUB_REF_NAME` is tag
+   1. Delete the original tag and re-tag it.
+   2. Generate a release, including the QR code and other information.
+6. If `GITHUB_REF_NAME` is branch
+   1. Push the changes to the branch.
 
-## Create an action from this template
+## Usage
 
-Click the `Use this Template` and provide the new repo details for your action
+### Basic Config
 
-## Code in Main
+```yml
+on:
+  push:
+    tags: [ v* ]
+  workflow_dispatch:
 
-> First, you'll need to have a reasonably modern version of `node` handy. This won't work with versions older than 9, for instance.
-
-Install the dependencies  
-```bash
-$ npm install
+jobs:
+  taro_release_job:
+    runs-on: ubuntu-latest
+    name: Taro Bundle Release
+    steps:
+      - name: Release Taro React Native bundle
+        uses: zhiqingchen/taro-react-native-release@v1
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          appname: Taro-Mortgage-calculator
+          logo: https://pic3.58cdn.com.cn/nowater/fangfe/n_v25b1523466b894881b9bdeda7618a8af2.png
 ```
 
-Build the typescript and package it for distribution
-```bash
-$ npm run build && npm run package
-```
+### Options
 
-Run the tests :heavy_check_mark:  
-```bash
-$ npm test
+see [action.yml](./action.yml) for details.
 
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
+### Examples
 
-...
-```
+0. [wuba/Taro-Mortgage-Calculator](https://github.com/wuba/Taro-Mortgage-Calculator/tree/master/.github/workflows/main.yml)
 
-## Change action.yml
+## Acknowledgements
 
-The action.yml defines the inputs and output for your action.
+We use [jsDelivr](https://www.jsdelivr.com/?docs=gh) as the cdn service.
+## LICENSE
 
-Update the action.yml with your name, description, inputs and outputs for your action.
-
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
-
-## Change the Code
-
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
-
-```javascript
-import * as core from '@actions/core';
-...
-
-async function run() {
-  try { 
-      ...
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run()
-```
-
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
-
-## Publish to a distribution branch
-
-Actions are run from GitHub repos so we will checkin the packed dist folder. 
-
-Then run [ncc](https://github.com/zeit/ncc) and push the results:
-```bash
-$ npm run package
-$ git add dist
-$ git commit -a -m "prod dependencies"
-$ git push origin releases/v1
-```
-
-Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
-
-Your action is now published! :rocket: 
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Validate
-
-You can now validate the action by referencing `./` in a workflow in your repo (see [test.yml](.github/workflows/test.yml))
-
-```yaml
-uses: ./
-with:
-  milliseconds: 1000
-```
-
-See the [actions tab](https://github.com/actions/typescript-action/actions) for runs of this action! :rocket:
-
-## Usage:
-
-After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
+The MIT License (MIT)
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,145 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as fse from 'fs-extra'
+import * as github from '@actions/github'
+import * as path from 'path'
+import QRCode from 'qrcode'
 
-async function run(): Promise<void> {
+interface BundleParama {
+  platform: string
+  bundlePath: string
+  qrPath: string
+  assetsDest: string
+  publicPath: string
+}
+
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // 1. get params from workflow
+    const env = process.env
+    core.info(`env: ${JSON.stringify(env, undefined, 2)}`)
+    const respository = env['GITHUB_REPOSITORY'] as string
+    const owner = env['GITHUB_REPOSITORY_OWNER'] as string
+    const payload = github.context.payload
+    core.info(`payload: ${JSON.stringify(payload, undefined, 2)}`)
+    const cdnhost = core.getInput('cdnhost')
+    const cdnpath = core.getInput('cdnpath')
+    const refname = (core.getInput('refname') || env['GITHUB_REF_NAME']) as string
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const publicPathPerfix = `${cdnpath}/${respository}@${refname}/`
+    const prefix = `${cdnhost}${publicPathPerfix}`
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const iosBundlePath = core.getInput('iosbundleoutput')
+    const iosQrPath = core.getInput('iosqrpath')
+    const iosAssetsDest = core.getInput('iosassetsdest')
+    const androidBundlePath = core.getInput('androidbundleoutput')
+    const androidQrPath = core.getInput('androidqrpath')
+    const androidAssetsDest = core.getInput('androidassetsdest')
+    const appName = core.getInput('appname')
+    const logo = core.getInput('logo')
+    const releaseprefix = core.getInput('releaseprefix')
+    const token = core.getInput('token')
+    const git = github.getOctokit(token)
+
+    const refType = env['GITHUB_REF_TYPE']
+
+    // 2. ios bundle params
+    const bundles: BundleParama[] = []
+    bundles.push({
+      platform: 'ios',
+      bundlePath: iosBundlePath,
+      qrPath: iosQrPath,
+      assetsDest: iosAssetsDest,
+      publicPath: `${publicPathPerfix}${iosAssetsDest}`
+    })
+
+    // 3. android bundle params
+    bundles.push({
+      platform: 'android',
+      bundlePath: androidBundlePath,
+      qrPath: androidQrPath,
+      assetsDest: androidAssetsDest,
+      publicPath: `${publicPathPerfix}${androidAssetsDest}`
+    })
+
+    // 4. run build bundle
+    for (const bundle of bundles) {
+      core.info(`bundle: ${JSON.stringify(bundle, undefined, 2)}`)
+      const {platform, bundlePath, qrPath, assetsDest, publicPath} = bundle
+      const sourcemapparms = getSourceMapParams(platform)
+      await exec.exec(`yarn build:rn --reset-cache --platform ${platform} --bundle-output ${bundlePath} --assets-dest ${assetsDest} --publicPath ${publicPath} ${sourcemapparms}`)
+      await exec.exec(`cp -rf ${assetsDest}${publicPath}/ ${assetsDest}`)
+      const bundleUrl = `${prefix}${bundlePath}`
+      core.info(`bundle url: ${bundleUrl}`)
+      const qrText = `taro://releases?platform=${platform}&url=${encodeURIComponent(bundleUrl)}&name=${encodeURIComponent(appName)}&logo=${encodeURIComponent(logo)}`
+      core.info(`qr text: ${qrText}`)
+      genQr(qrText, qrPath)
+    }
+
+    // 5. git commit
+    await exec.exec(`git config --global user.name "${payload.pusher?.name || payload.sender?.login || 'unknown Author'}"`)
+    await exec.exec(`git config --global user.email "${payload.pusher?.email || 'unknown@unknown.unknown'}"`)
+    await exec.exec(`git status`)
+    await exec.exec(`git add .`)
+    await exec.exec(`git commit -m "release: commit by github actions"`)
+
+    if (refType === 'tag') {
+      // 6. reset tag
+      await exec.exec(`git tag -d ${refname}`)
+      await exec.exec(`git push origin :refs/tags/${refname}`)
+      await exec.exec(`git tag ${refname}`)
+      await exec.exec(`git push origin ${refname}`)
+
+      // 7. upload release
+      git.rest.repos.createRelease({
+        body: `${releaseprefix}
+
+||  Android  |  iOS  |
+| :--: | :--: | :--: |
+| QR code | ![AndroidBundle](${prefix}${androidQrPath}) | ![iOSBundle](${prefix}${iosQrPath}) |
+| Bundle file url | ${prefix}${androidBundlePath} | ${prefix}${iosBundlePath} |
+
+  `,
+        owner,
+        repo: respository.replace(`${owner}/`, ''),
+        tag_name: refname
+      })
+    } else if (refType === 'branch') {
+      // 6. push branch
+      await exec.exec(`git push origin`)
+      core.info(`open ${prefix}${androidQrPath} , and use the Taro Playground Android APP to san for prview android release.`)
+      core.info(`open ${prefix}${iosQrPath} , and use the Taro Playground iOS APP to san for prview ios release.`)
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    core.setFailed(error)
   }
+}
+
+function getSourceMapParams(platform: string): string {
+  const result: string[] = []
+  const sourcemapOutput = core.getInput(`${platform}sourcemapoutput`)
+  const sourcemapUseAbsolutePath = core.getInput(`${platform}sourcemapuseabsolutepath`)
+  const sourcemapSourcesRoot = core.getInput(`${platform}sourcemapsourcesroot`)
+  if (sourcemapOutput) {
+    result.push(`--sourcemap-output ${sourcemapOutput}`)
+  }
+  if (sourcemapUseAbsolutePath) {
+    result.push(`--sourcemap-use-absolute-path ${sourcemapUseAbsolutePath}`)
+  }
+  if (sourcemapSourcesRoot) {
+    result.push(`--sourcemap-sources-root ${sourcemapSourcesRoot}`)
+  }
+  return result.join(' ')
+}
+
+function genQr(text: string, dist: string): void {
+  const QR_CODE_PNG_PATH = `${process.cwd()}/${dist}`
+  fse.ensureDirSync(path.dirname(QR_CODE_PNG_PATH))
+  QRCode.toFile(QR_CODE_PNG_PATH, text, {type: 'png'}, err => {
+    if (err) throw err
+    core.info(`generated: ${text} to ${dist}`)
+  })
 }
 
 run()
